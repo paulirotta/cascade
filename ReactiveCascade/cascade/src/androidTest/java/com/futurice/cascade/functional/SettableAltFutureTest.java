ee59b14af505740ec9da8f0fcf9512b795762dbb
Paul Houghton <paul.houghton@futurice.com>
2015-05-29 14:14:56 +0200

    =PMD some config

---
+++ ReactiveCascade/cascade/src/androidTest/java/com/futurice/cascade/functional/SettableAltFutureTest.java
@@ -0,0 +1,16 @@
+package com.futurice.cascade.functional;
+
+import android.test.suitebuilder.annotation.LargeTest;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by phou on 5/29/2015.
+ */
+public class SettableAltFutureTest extends TestCase {
+
+    @LargeTest
+    public void testCancel() throws Exception {
+
+    }
+}
